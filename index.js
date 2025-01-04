30f2274f1a00e14e37b989e4f49d04ea9a52c89d
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2025-01-04 21:36:55 +0100

    add

---
+++ index.js
@@ -0,0 +1,43 @@
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require(".//src/contacts");
+const { Command } = require("commander");
+const program = new Command();
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "contact id")
+  .option("-n, --name <type>", "contact name")
+  .option("-e, --email <type>", "contact email")
+  .option("-p, --phone <type>", "contact phone");
+
+program.parse(process.argv);
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      console.table(await listContacts());
+      break;
+
+    case "get":
+      console.log(await getContactById(id));
+      break;
+
+    case "add":
+      console.log(await addContact(name, email, phone));
+      break;
+
+    case "remove":
+      console.table(await removeContact(id));
+      break;
+
+    default:
+      console.warn("❌ Unknown action!");
+  }
+}
+
+invokeAction(argv);
