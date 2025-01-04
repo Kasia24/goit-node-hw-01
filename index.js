6b2c8dcbc1a100dd3137b14c3d9a285a3839547c
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2025-01-04 19:01:52 +0100

    add

---
+++ index.js
@@ -0,0 +1,22 @@
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+} = require("./contacts");
+
+async function main() {
+  console.log("Lista kontaktów:");
+  console.table(await listContacts());
+
+  console.log("Dodaj kontakt:");
+  console.log(await addContact("Mango", "mango@gmail.com", "322-22-22"));
+
+  console.log("Pobierz kontakt po ID:");
+  console.log(await getContactById("1"));
+
+  console.log("Usuń kontakt po ID:");
+  console.table(await removeContact("1"));
+}
+
+main();
